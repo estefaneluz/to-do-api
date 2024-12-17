d4097d53fb83b4126947d0f2de8f413d8b3b17a7
estefane.cortes <estefane.cortes@aquare.la>
2024-12-16 21:24:43 -0300

    feat: create tasks serializers

---
+++ tasks/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import TaskItem, Tag
+from users.models import User
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'hex_color']
+
+class TaskItemSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, required=False)
+    created_by = serializers.SerializerMethodField()
+
+    class Meta:
+        model = TaskItem
+        fields = ['id', 'title', 'status', 'tags', 'created_by', 'is_active', 'created_at', 'updated_at', 'deleted_at']
+        read_only_fields = ['created_at', 'updated_at', 'deleted_at']
+
+    def get_created_by(self, obj):
+        return obj.created_by.name
